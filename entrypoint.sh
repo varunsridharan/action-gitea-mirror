912f84f0c3f789ce5b92ece1a863062c8c9c4951
Varun <varunsridharan23@gmail.com>
2020-10-16 20:49:02 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,7 @@ echo "###[group] Setting Up Config"
 template='{"github":{"username":"%s","accesstoken":"%s"},"gitea":{"host":"%s","accesstoken":"%s","username":"%s","default_userpassword":"%s","gist":{"prefix":"%s","surfix":"%s"}},"repomap":{},"gistsSource":%s,"gistsStared":%s,"repositoryStared":%s,"repositorySource":%s,"repositoryForked":%s}'
 json_string=$(printf "$template" "$gh_username" "$accesstoken" "$gitea_host" "$gitea_accesstoken" "$gitea_username" "RY7VpBD62P7964c9mSz%2GAM82hv68yUbHc4@Gdw8c%f^W3*qdzof*8a6b^rKy" "$gitea_gist_prefix" "$gitea_gist_surfix" "$gistsSource" "$gistsStared" "$repositoryStared" "$repositorySource" "$repositoryForked")
 echo "$json_string" >> ./mirror-handler/src/config.json
+cat ./mirror-handler/src/config.json
 echo "###[endgroup]"
 
 echo "###[group] Setting Up Mirror For {Source Gists}"
